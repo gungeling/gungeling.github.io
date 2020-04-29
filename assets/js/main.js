6dfad5dc3bb28ceb4f68bc119b4adfa002ef71dd
surya creator <cokdefajus999@gmail.com>
2020-04-29 10:10:05 +0800

    small problem

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,6 @@
 $(document).ready(function(){
 	$('.toggle-nav-dropdown').on('click', function(){
         var show_nav = $(this).attr('alt');
-
         $('.header .'+show_nav+' ul').slideToggle();
         $('.header #toggle-nav-chevron').toggleClass('rotate-toggle');
     });
